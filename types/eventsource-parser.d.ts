177af7a8b158632e04aef51a6c6039cb0b7833c3
intelligentech <125271978+intelligentech@users.noreply.github.com>
2024-09-11 01:29:45 +1000

    Create eventsource-parser.d.ts

---
+++ types/eventsource-parser.d.ts
@@ -0,0 +1,18 @@
+declare module 'eventsource-parser' {
+    export function createParser(onParse: (event: ParsedEvent | ReconnectInterval) => void): {
+      feed: (chunk: string) => void;
+      reset: () => void;
+    };
+  
+    export type ParsedEvent = {
+      type: 'event';
+      data: string;
+      event?: string;
+      id?: string;
+    };
+  
+    export type ReconnectInterval = {
+      type: 'reconnect-interval';
+      value: number;
+    };
+  }
