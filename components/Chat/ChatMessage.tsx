e26b45e9456129f9352b3006fd462d0a5a8fad83
intelligentech <125271978+intelligentech@users.noreply.github.com>
2024-12-12 06:35:00 +1100

    Update ChatMessage.tsx

--- components/Chat/ChatMessage.tsx
+++ components/Chat/ChatMessage.tsx
@@ -11,8 +11,8 @@ export const ChatMessage: FC<Props> = ({ message }) => {
       <div
         className={`flex items-center ${
           message.role === "assistant" 
-            ? "bg-[#f7e1ff] bg-opacity-85 text-neutral-900" 
-            : "bg-blue-500 bg-opacity-85 text-white"
+            ? "bg-[#f7e1ff] bg-opacity-60 text-neutral-900" 
+            : "bg-blue-500 bg-opacity-60 text-white"
         } rounded-2xl px-3 py-2 max-w-[67%] whitespace-pre-wrap`}
         style={{ overflowWrap: "anywhere" }}
       >
