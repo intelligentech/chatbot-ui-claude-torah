0b2ebc24313777a1473a6577e8bf8e8ba7e6f310
intelligentech <125271978+intelligentech@users.noreply.github.com>
2024-12-12 06:33:22 +1100

    Update ChatMessage.tsx
    Added msg opacity

--- components/Chat/ChatMessage.tsx
+++ components/Chat/ChatMessage.tsx
@@ -9,7 +9,11 @@ export const ChatMessage: FC<Props> = ({ message }) => {
   return (
     <div className={`flex flex-col ${message.role === "assistant" ? "items-start" : "items-end"}`}>
       <div
-        className={`flex items-center ${message.role === "assistant" ? "bg-[#f7e1ff] text-neutral-900" : "bg-blue-500 text-white"} rounded-2xl px-3 py-2 max-w-[67%] whitespace-pre-wrap`}
+        className={`flex items-center ${
+          message.role === "assistant" 
+            ? "bg-[#f7e1ff] bg-opacity-85 text-neutral-900" 
+            : "bg-blue-500 bg-opacity-85 text-white"
+        } rounded-2xl px-3 py-2 max-w-[67%] whitespace-pre-wrap`}
         style={{ overflowWrap: "anywhere" }}
       >
         {message.content}
