9b4b42ec81aa54be1f1e218158c62439469508b7
intelligentech <125271978+intelligentech@users.noreply.github.com>
2025-11-16 03:27:33 +1100

    Update Navbar.tsx

--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -10,7 +10,7 @@ export const Navbar: FC = () => {
         >
           <span className="whitespace-nowrap"> {'\u{1F4AC}'}{'\u{1F310}'}{'\u{1F525}'} AI Eliyahu HaNavi </span>
           <br className="sm:hidden" />
-          <span className="whitespace-nowrap sm:inline-block sm:ml-1">(Elijah The Prophet) <span className="text-lg">(v0.7.0-Jun2025)</span></span>
+          <span className="whitespace-nowrap sm:inline-block sm:ml-1">(Elijah The Prophet) <span className="text-lg">(v1.0.0-Nov2025)</span></span>
         </a>
       </div>
     </div>
