2d312b67a018a89fbb077ee9eaf9785ce0906870
Brendan Cilia <intelligentechdev@gmail.com>
2025-12-10 13:56:40 +1100

    Add Rabbi Eliyahu image to navbar (90x90, centered)

--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -8,6 +8,15 @@ export const Navbar: FC = () => {
           className="hover:opacity-50"
           href="https://eliyahu.chat"
         >
+          <div className="flex justify-center mb-2">
+            <img 
+              src="https://unstoppablelinks.com/images/rabbieliyahu.webp" 
+              alt="Rabbi Eliyahu" 
+              width="90" 
+              height="90"
+              className="rounded"
+            />
+          </div>
           <span className="whitespace-nowrap"> {'\u{1F4AC}'}{'\u{1F310}'}{'\u{1F525}'} AI Rabbi Eliyahu</span>
           <br className="sm:hidden" />
           <span className="whitespace-nowrap sm:inline-block sm:ml-1">of AskJudaism.org <span className="text-lg">(v5.1.4-Dec2025)</span></span>
